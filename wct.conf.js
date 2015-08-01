c2d074fdf9952190ded1c024c9a0b74521ad065c
Pierre Verkest <pverkest@anybox.fr>
2015-08-02 00:22:59 +0200

    sauce declared as pluggin, the other method was deprecated

--- wct.conf.js
+++ wct.conf.js
@@ -1,61 +1,63 @@
 module.exports = {
   suites: ['./www/elements/c2m-admin-app/test/',
            './www/elements/c2m-video-playlist/test/'],
-  sauce: {
-    browsers: [
-      {
-        "browserName": "internet explorer",
-        "platform":    "Windows 8.1",
-        "version":     "11"
-      },
-      {
-        "browserName": "internet explorer",
-        "platform":    "Windows 7",
-        "version":     "11"
-      },
+  plugins: {
+    sauce: {
+      browsers: [
+        {
+          "browserName": "internet explorer",
+          "platform":    "Windows 8.1",
+          "version":     "11"
+        },
+        {
+          "browserName": "internet explorer",
+          "platform":    "Windows 7",
+          "version":     "11"
+        },
 
-      {
-        "browserName": "chrome",
-        "platform":    "Windows 8.1",
-        "version":     "beta"
-      },
-      {
-        "browserName": "chrome",
-        "platform":    "Windows 8.1",
-        "version":     ""
-      },
-      {
-        "browserName": "chrome",
-        "platform":    "OS X 10.10",
-        "version":     ""
-      },
-      {
-        "browserName": "chrome",
-        "platform":    "Linux",
-        "version":     ""
-      },
+        {
+          "browserName": "chrome",
+          "platform":    "Windows 8.1",
+          "version":     "beta"
+        },
+        {
+          "browserName": "chrome",
+          "platform":    "Windows 8.1",
+          "version":     ""
+        },
+        {
+          "browserName": "chrome",
+          "platform":    "OS X 10.10",
+          "version":     ""
+        },
+        {
+          "browserName": "chrome",
+          "platform":    "Linux",
+          "version":     ""
+        },
 
-      {
-        "browserName": "firefox",
-        "platform":    "Windows 8.1",
-        "version":     ""
-      },
-      {
-        "browserName": "firefox",
-        "platform":    "OS X 10.10",
-        "version":     ""
-      },
-      {
-        "browserName": "firefox",
-        "platform":    "Linux",
-        "version":     ""
-      },
+        {
+          "browserName": "firefox",
+          "platform":    "Windows 8.1",
+          "version":     ""
+        },
+        {
+          "browserName": "firefox",
+          "platform":    "OS X 10.10",
+          "version":     ""
+        },
+        {
+          "browserName": "firefox",
+          "platform":    "Linux",
+          "version":     ""
+        },
 
-      {
-        "browserName": "safari",
-        "platform":    "OS X 10.9",
-        "version":     "7"
-      }
-    ]
+        {
+          "browserName": "safari",
+          "platform":    "OS X 10.9",
+          "version":     "7"
+        }
+      ]
+    }
   }
 };
